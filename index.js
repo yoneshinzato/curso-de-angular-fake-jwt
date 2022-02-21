4cc8cc73ae096bf5a81220aa45971ee33b417895
Dener Troquatte <you@example.com>
2022-02-21 17:49:24 -0300

    Jwt

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post("/sign", (req, res) => {
+  const email = "dener@vidafullstack.com.br";
+  const password = "123";
+
+  const data = {
+    nome: "Dener Troquatte",
+    email,
+    role: ["sysAdmin"],
+  };
+
+  if (req.body.user === email && req.body.password === password) {
+    const token = jwt.sign({ data }, "SECRET", {
+      expiresIn: 300,
+    });
+    return res.json({ token: token });
+  }
+
+  res.status(500).json({ message: "Login inválido!" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
